f8ed2acc48e4ae4ee664895c8e34ff6c4b6f3552
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-01-12 14:31:43 -0300

    Time Intervals Page

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -5,7 +5,7 @@ import { ArrowRight, Check } from 'phosphor-react'
 import { Container, Header } from '../styles'
 import { AuthError, ConnectBox, ConnectItem } from './style'
 
-export default function Register() {
+export default function ConnectCalendar() {
   const session = useSession()
   const router = useRouter()
 
