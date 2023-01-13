5fc11f14926b0e9c0c3de99d7acff7a51bf253ed
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-01-13 10:58:59 -0300

    Creating model UserTimeInterval

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -10,6 +10,7 @@ import {
 import { ArrowRight } from 'phosphor-react'
 import { Controller, useFieldArray, useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { api } from '../../../lib/axios'
 import { convertTimeStringToMInutes } from '../../../utils/conver-time-string-to-minutes'
 import { getWeekDays } from '../../../utils/get-week-days'
 import { Container, Header } from '../styles'
@@ -99,10 +100,12 @@ export default function TimeIntervals() {
    * it suposed to be:
    * function handleSetTimeIntervals(data: TimeIntervalsFormOutput) {}
    */
-  function handleSetTimeIntervals(data: any) {
-    const formData = data as TimeIntervalsFormOutput
+  async function handleSetTimeIntervals(data: any) {
+    const { intervals } = data as TimeIntervalsFormOutput
 
-    console.log(formData)
+    await api.post('/users/time-intervals', {
+      intervals,
+    })
   }
 
   return (
